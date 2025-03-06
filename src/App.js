bdfd9391046b0529bcef03102b7636a70ab95204
Hector Diaz <hdiaz@nectia.com>
2025-03-06 11:09:50 -0300

    feat(clase-3): integrate Redux with initial store setup and update dependencies

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { Provider } from 'react-redux';
 import './App.css';
 import Routes from './routes';
+import store from './redux/store';
+
 
 function App() {
-  return <Routes />;
+  return (
+    <Provider store={store}>
+      <Routes />
+    </Provider>
+  );
 }
 
 export default App;
